4bb69d1a4a758f71cd0773843a8b0b825075ba64
Mustafa Aljadery <mustafaaljadery1@gmail.com>
2023-10-12 11:29:21 -0700

    add analytics

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import React from "react";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
