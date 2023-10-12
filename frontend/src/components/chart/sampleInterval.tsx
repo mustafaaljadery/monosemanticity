44feed68418246f91fdf5c071493e6a3986377cf
Mustafa Aljadery <mustafaaljadery1@gmail.com>
2023-10-11 19:24:59 -0700

    fix error

--- frontend/src/components/chart/sampleInterval.tsx
+++ frontend/src/components/chart/sampleInterval.tsx
@@ -9,7 +9,7 @@ export default function SampleInterval({ data, searchQuery }: Props) {
   return (
     <div className="w-1/3 flex flex-col p-4">
       <p className="text-xs font-semibold text-[#363636]">
-        {data.quantile_name} - Max Act: {data.max_act}
+        {data?.quantile_name} - Max Act: {data?.max_act}
       </p>
       <div className="flex flex-col space-y-1 mt-3">
         {data?.examples?.map((example: any, index: number) => {
