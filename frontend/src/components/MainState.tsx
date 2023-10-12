63bc13e35e01605eac07c788645e260cb1eaec21
Mustafa Aljadery <mustafaaljadery1@gmail.com>
2023-10-11 20:27:15 -0700

    fix homepage

--- frontend/src/components/MainState.tsx
+++ frontend/src/components/MainState.tsx
@@ -16,16 +16,16 @@ export default function MainState({
 }: Props) {
   return (
     <div className="w-[90%] md:w-3/4 lg:w-2/3 xl:w-1/2 mt-32 flex flex-col justify-center items-center space-y-4">
-      <h1 className="text-5xl font-bold text-[#363636] text-center mb-8">
+      <h1 className="text-4xl md:text-5xl font-bold text-[#363636] text-center mb-8">
         Monosemanticity Search<span className="text-[#CB785C]">.</span>
       </h1>
       <div className="flex flex-col w-full rounded-lg bg-[#F0F0EA] p-12">
         <div className="text-center justify-center items-center flex flex-row space-x-2.5 flex-wrap">
-          <p className="font-medium text-[#363636] text-2xl">
+          <p className="font-medium text-[#363636] text-xl md:text-2xl">
             I want to see the activations for
           </p>
           <input
-            className="font-regular p-1 rounded focus:ring-0 focus:outline-[#CD9D7B]"
+            className="font-regular text-sm md:text-base p-1 rounded focus:ring-0 focus:outline-[#CD9D7B]"
             placeholder="word..."
             type="text"
             value={search}
@@ -34,11 +34,11 @@ export default function MainState({
               setSearchState(true);
             }}
           />
-          <p className="font-medium text-[#363636] text-2xl">
+          <p className="font-medium text-[#363636] text-xl md:text-2xl">
             and learn what&apos;s going on in a neural network.
           </p>
         </div>
-        <div className="flex mt-5 text-sm font-regular text-gray-500 flex-row flex-wrap justify-center items-center text-center space-x-1">
+        <div className="flex mt-5 text-xs md:text-sm font-regular text-gray-500 flex-row flex-wrap justify-center items-center text-center space-x-1">
           <p>Created by</p>
           <a
             rel="noopenner noreferrer"
@@ -58,30 +58,35 @@ export default function MainState({
             Siddharth.
           </a>
           <p>Indexed using </p>
-          <img
-            height={20}
-            width={20}
-            className="my-auto p-[2px]"
-            src="/redis.svg"
-          />
-          <p>, and visualized using</p>
-          <img
-            height={20}
-            width={20}
-            src="/d3.png"
-            className="my-auto p-[2px]"
-          />
-          <p>by</p>
-          <img
-            height={20}
-            width={20}
-            src="/observable.png"
-            className="my-auto p-[2px]"
-          />
-          <p>.</p>
-          <p>We&apos;ve also visualized Anthropic&apos;s paper, check it out</p>
           <a
-            href="https://www.maxaljadery.com/writing"
+            href="https://redis.com/"
+            target="_blank"
+            rel="noopenner noreferrer"
+            className="my-auto hover:opacity-80"
+          >
+            <img
+              height={20}
+              width={20}
+              className="my-auto p-[2px]"
+              src="/redis.svg"
+            />
+          </a>
+          <p>, with the data from</p>
+          <a
+            href="https://transformer-circuits.pub/2023/monosemantic-features/vis/index.html"
+            target="_blank"
+            rel="noopenner noreferrer"
+            className="text-[#004276] hover:opacity-70"
+          >
+            Anthropic&apos;s A/1
+          </a>
+          <p>dictioary learning run.</p>
+          <p>
+            We&apos;ve also simplified the original paper with visuals, check it
+            out
+          </p>
+          <a
+            href="https://www.maxaljadery.com/posts/decomposing-monosemanticity"
             target="_blank"
             rel="noopenner noreferrer"
             className="text-[#004276] hover:opacity-70"
@@ -90,7 +95,7 @@ export default function MainState({
           </a>
         </div>
       </div>
-      <div className="flex justify-center items-center text-sm text-gray-500 flex-wrap flex-row pt-6 space-x-1">
+      <div className="flex justify-center pb-24 items-center text-xs md:text-sm text-gray-500 flex-wrap flex-row pt-8 space-x-1">
         <a
           href="https://transformer-circuits.pub/2023/monosemantic-features/index.html"
           target="_blank"
